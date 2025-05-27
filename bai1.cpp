d3c62a8f4375e2be5a052b0d5b8728d78336ce8b
vo duy tan <voduytan1632005@gmail.com>
2025-05-28 00:30:57 +0700

    init project

---
+++ bai1.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+#define bi 3.141593
+float r(float S){
+	float R;
+	return R=sqrt(S/(4*bi));
+}
+void V(float S){
+	float V;
+	float R=r(S);
+	V=((float(4)/3)*bi*(R*R*R));
+	printf("The Tich V: %f ",V);
+}
+int main(){
+	float S;
+	printf("nhap vao dien tich S:  ");
+	scanf("%f",&S);
+	V(S);
+	return 0;
+}
